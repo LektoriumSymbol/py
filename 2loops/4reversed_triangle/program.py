539bd8751701304b234c83d057c38825c4f1d5c4
LektoriumSymbol <74270066+LektoriumSymbol@users.noreply.github.com>
2021-11-02 22:53:29 +0200

    Add reversed triangle to the loops chapter

---
+++ 2loops/4reversed_triangle/program.py
@@ -0,0 +1,15 @@
+print("* * * Reversed triangle looks like this * * *")
+print("Enter vertical count")
+vertical = int(input())
+
+horizontal_increment = 0
+vertical_increment = 0
+
+while(vertical_increment < vertical):
+    horizontal_increment = vertical - vertical_increment
+    while(horizontal_increment > 0):
+        print("* ", end="")
+        horizontal_increment -= 1
+    
+    print()
+    vertical_increment += 1
