f3acdcb7d028a44b2e0a42dde85731b3c1131270
LektoriumSymbol <74270066+LektoriumSymbol@users.noreply.github.com>
2021-11-03 19:42:00 +0200

    Add factorial to the functions chapter

---
+++ 3functions/2factorial/program.py
@@ -0,0 +1,15 @@
+print("* * * This is your calculator * * *")
+
+def factorial(n):
+    if(n == 1): return n
+
+    return n * factorial(n - 1)
+
+while(True):
+    print("> Number to calculate: ", end="")
+    n = int(input())
+
+    print(factorial(n))
+    print()
+
+    
