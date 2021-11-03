72a563db86939743a30baf774e5dc9e479b65bfc
LektoriumSymbol <74270066+LektoriumSymbol@users.noreply.github.com>
2021-11-03 18:58:18 +0200

    Add Elephant to the loops chapter

---
+++ 2loops/01elephant/program.py
@@ -0,0 +1,20 @@
+print("* * * This is your first program * * *")
+print("> Hello! What is your name?")
+name = input("> ")
+print("> Hi " + name + "! How are you??")
+my_shape = input("> ")
+print("> Well, " + name + ", nice to hear that you are " + my_shape + "!")
+print()
+print("> Say.. %s, do you want to by an elephant??" % name)
+
+flag = True
+while(flag == True):
+    my_best_answer = input("> ")
+
+    if my_best_answer.lower() == "yes":
+        print("> Relax man, just a joke just a joke... chill")
+        flag = False
+    else:
+        print('''> No way bro, everyone says "%(0)s", but you gonna try this, I'm telling ya %(1)s''' % {"0": my_best_answer, "1": name})
+
+print()
