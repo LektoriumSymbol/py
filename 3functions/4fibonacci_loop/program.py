d4dda2fa2841cddfd73fe207c12ca00d7c3c006f
LektoriumSymbol <74270066+LektoriumSymbol@users.noreply.github.com>
2021-11-04 19:07:38 +0200

    Add fibonacci set via loop with semantic sugar

---
+++ 3functions/4fibonacci_loop/program.py
@@ -0,0 +1,14 @@
+print("* * * This is your fibonacci set via loop with semantic sygar * * *")
+
+N = int(input("> Enter number of elements in Fibonacci series, please, N : "))
+
+def fib(n):
+    a, b = 0, 1
+    
+    while a <= n:
+        print(a, end=' ')
+        a, b = b, a+b
+    
+    print()
+
+fib(N)
