1b49694e63d32a91ec475d8bc2be38460b90dc6f
LektoriumSymbol <74270066+LektoriumSymbol@users.noreply.github.com>
2021-11-05 20:16:07 +0200

    Add calculator to the objects chapter

---
+++ 5objects/1calculator/program.py
@@ -0,0 +1,49 @@
+print("* * * This is your calculator * * *")
+
+class Calculator:
+    
+    def calculate(self, a, b, sign):
+        result = 0
+
+        match sign:
+                case "+":
+                    result = self.sum(a, b)
+                case "-":
+                    result = self.sub(a, b)
+                case "*":
+                    result = self.mul(a, b)
+                case "/":
+                    if(b != 0):
+                        result = self.div(a, b)
+                    else:
+                        print("! Can't be devided by zero")
+                case _:
+                    print("! Non reserved action")
+
+        print("> Result is: %s" % result)
+        print()
+
+    def sum(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a , b):
+        return a / b
+
+
+
+while(True):
+    print("> First input: ", end="")
+    a = int(input())
+    print("> Second input: ", end="")
+    b = int(input())
+    print("> Action: ", end="")
+    sign = input()
+
+    calculator = Calculator()
+    calculator.calculate(a, b, sign) 
