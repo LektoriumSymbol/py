1922d92320865eb012f25cb4b041f347c62ab818
LektoriumSymbol <74270066+LektoriumSymbol@users.noreply.github.com>
2021-11-05 21:19:09 +0200

    Add prime numbers checker to the objects chapter

---
+++ 5objects/2prime/program.py
@@ -0,0 +1,22 @@
+print("* * * This is your prime number checker * * *")
+
+class Checker:
+    
+    def is_prime(num):
+        is_prime = False
+        if num > 1:
+            for i in range(2, int(num/2)+1):           
+                if (num % i) == 0:
+                    break
+            else:
+                is_prime = True
+
+        return is_prime
+
+
+while(True):
+    print("> Type in your number to check : ", end="")
+    x = int(input())
+
+    print(Checker.is_prime(x) )
+    print()
