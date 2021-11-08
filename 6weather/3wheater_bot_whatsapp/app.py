c7cb0a126dc106db6c3f8fd0e37899a20ba4e2b2
LektoriumSymbol <74270066+LektoriumSymbol@users.noreply.github.com>
2021-11-08 22:53:41 +0200

    Update whatsapp weather bot

--- 6weather/3wheater_bot_whatsapp/app.py
+++ 6weather/3wheater_bot_whatsapp/app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from pyowm import OWM
+from pyowm.utils.config import get_default_config
 
 app = Flask(__name__)
+config_dict = get_default_config()
+config_dict['language'] = 'en'
+
+owm = OWM('fcdb2136605493641a45858f52a39d97', config_dict)
 
 @app.route("/")
 def hello():
@@ -9,10 +15,45 @@ def hello():
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
-    msg = request.form.get('Body')
 
+    msg = request.form.get('Body')
+    mgr = owm.weather_manager()
     resp = MessagingResponse()
-    resp.message("Your massage: {}".format(msg))
+
+    try:
+        observation = mgr.weather_at_place(msg)
+        w = observation.weather
+
+        t = w.temperature("celsius")
+        t1 = t['temp']
+        t2 = t['feels_like']
+        t3 = t['temp_max']
+        t4 = t['temp_min']
+
+        wi = w.wind()['speed']
+        humi = w.humidity
+        cl = w.clouds
+        st = w.status
+        dt = w.detailed_status
+        ti = w.reference_time('iso')
+        pr = w.pressure['press']
+        vd = w.visibility_distance
+
+        responce = ("In " + str(msg) + " temperature " + str(t1) + " °C" + "\n" + 
+            "Max temperature " + str(t3) + " °C" +"\n" + 
+            "Min temperature " + str(t4) + " °C" + "\n" + 
+            "Feels like " + str(t2) + " °C" + "\n" +
+            "Wind " + str(wi) + " m/s" + "\n" + 
+            "Atmospheric Pressure " + str(pr) + " ft" + "\n" + 
+            "Humidity " + str(humi) + " %" + "\n" + 
+            "Visibility distance " + str(vd) + "  m" + "\n" +
+            "Overview " + str(st) + "\n\n" + str(dt))
+
+    
+    except Exception:
+        responce = "City wasn't found :'("
+
+    resp.message("{}".format(responce))
 
     return str(resp)
 
